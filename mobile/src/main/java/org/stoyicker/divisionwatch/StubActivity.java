389e3babb5531a33a39149005d063c8c14adb8cd
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@schibsted.com>
2016-11-26 17:01:48 +0100

    Complete setup.

---
+++ mobile/src/main/java/org/stoyicker/divisionwatch/StubActivity.java
@@ -0,0 +1,6 @@
+package org.stoyicker.divisionwatch;
+
+import android.app.Activity;
+
+public final class StubActivity extends Activity {
+}
---
+++ wear/src/main/java/org/stoyicker/divisionwatch/DivisionWatchCWFService.java
@@ -0,0 +1,6 @@
+package org.stoyicker.divisionwatch;
+
+import android.support.wearable.watchface.CanvasWatchFaceService;
+
+public class DivisionWatchCWFService extends CanvasWatchFaceService {
+}
